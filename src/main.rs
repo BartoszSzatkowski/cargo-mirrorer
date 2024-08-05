13f994f69c649ea027bff44f20b6208d7d13a382
Bartosz Szatkowski <szatek24@gmail.com>
2024-08-05 21:26:48 +0200

    test: clone crates.io index using gix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gix::diff::object::bstr::BStr;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 fn init_logger() {
@@ -14,6 +15,33 @@ fn init_logger() {
 
 fn main() -> anyhow::Result<()> {
     init_logger();
+
+    let url = gix::url::parse(BStr::new(
+        "https://github.com/rust-lang/crates.io-index.git",
+    ))?;
+
+    println!("Url: {:?}", url.to_bstring());
+    let mut prepare_clone = gix::prepare_clone(url, &"./banana")?;
+
+    println!("Cloning crates index into here ...");
+    let (mut prepare_checkout, _) = prepare_clone
+        .fetch_then_checkout(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
+
+    println!(
+        "Checking out into {:?} ...",
+        prepare_checkout.repo().work_dir().expect("should be there")
+    );
+
+    let (repo, _) =
+        prepare_checkout.main_worktree(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?;
+    println!(
+        "Repo cloned into {:?}",
+        repo.work_dir().expect("directory pre-created")
+    );
+
+    let _remote = repo
+        .find_default_remote(gix::remote::Direction::Fetch)
+        .expect("always present after clone")?;
 
     Ok(())
 }
